1b5d23b66adfb777e4c30fc95f0508ea801df763
Andrew Marshall <andrew@johnandrewmarshall.com>
2011-11-06 14:31:49 -0500

    Include libraries needed on Linux

--- Project2/Parser.cpp
+++ Project2/Parser.cpp
@@ -1,6 +1,8 @@
 #include <assert.h>
 #include <stack>
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string>
 #include "Element.hpp"
 #include "Parser.hpp"
--- Project2/String.cpp
+++ Project2/String.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 #include "String.hpp"
 
 xml::String::String() {
