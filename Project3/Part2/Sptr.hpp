4fd2ae5c81dfb3b871f64adf4e9c2f671b8912c3
Andrew Marshall <andrew@johnandrewmarshall.com>
2011-12-16 15:14:35 -0500

    WIP

--- Project3/Part2/Sptr.hpp
+++ Project3/Part2/Sptr.hpp
@@ -2,30 +2,73 @@ namespace cs540 {
   template <typename T>
   class Sptr {
     private:
+      class SharedPointer {
+        private:
+          int refCount;
+
+        public:
+          SharedPointer(T& o) : refCount(0), object(o) {}
+          void increment() { ++refCount; }
+          void decrement() { if(--refCount == 0) delete object; }
+
+          T* object;
+      };
 
     public:
-      Sptr();
-      template <typename U> Sptr(U*);
+      SharedPointer* sharedPtr;
 
-      Sptr& operator=(const Sptr&);
-      template <typename U> Sptr<T>& operator=(const Sptr<U>&);
+      Sptr();
+
+      template <typename U>
+      Sptr(U*);
+
+      Sptr(const Sptr& s) : sharedPtr(s.sharedPtr) {
+        sharedPtr->increment();
+      };
+
+      template <typename U>
+      Sptr(const Sptr<U>& s) : sharedPtr(static_pointer_cast<Sptr<T>>(s).sharedPtr) {
+        sharedPtr->increment();
+      }
+
+      Sptr& operator=(const Sptr& s) {
+        if(!(*this == s)) {
+          sharedPtr->decrement();
+          sharedPtr = s.sharedPtr;
+        }
+        sharedPtr->increment();
+        return *this;
+      }
+
+      template <typename U>
+      Sptr<T>& operator=(const Sptr<U>& s) {
+        s = static_pointer_cast<Sptr<T>>(s);
+        sharedPtr->decrement();
+        sharedPtr = s.sharedPtr;
+        sharedPtr->increment();
+        return *this;
+      }
 
       void reset();
 
-      T& operator*() const;
-      T* operator->() const;
+      T& operator*() const { return *(sharedPtr->object); }
+      T* operator->() const { return sharedPtr->object; }
 
-      T* get() const;
+      T* get() const { return sharedPtr->object; }
 
-      operator unspecified_bool_type() const;
+      operator unspecified_bool_type() const {
+        return false; //FIXME
+      }
   };
 
   template <typename T1, typename T2>
   bool operator==(const Sptr<T1>&, const Sptr<T2>&);
 
   template <typename T, typename U>
-  Sptr<T> static_pointer_cast(const Sptr<U>&);
+  Sptr<T> static_pointer_cast(const Sptr<U>& s) {
+    s.sharedPtr->object static_cast<T>(s.)
+  }
 
   template <typename T, typename U>
-  Sptr<T> dynamic_pointer_case(const Sptr<U>&);
+  Sptr<T> dynamic_pointer_cast(const Sptr<U>&);
 }
