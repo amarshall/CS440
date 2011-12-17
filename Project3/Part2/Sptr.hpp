b75cdfc5272b9bfbc68370a4aabcef915f47a844
Andrew Marshall <andrew@johnandrewmarshall.com>
2011-12-16 20:02:36 -0500

    WIP

--- Project3/Part2/Sptr.hpp
+++ Project3/Part2/Sptr.hpp
@@ -1,63 +1,72 @@
 namespace cs540 {
+  class SharedCounter {
+    public:
+      SharedCounter() : refCount(0) {}
+      int refCount;
+      int decrement() { if(refCount > 0) { --refCount; } return refCount; }
+      int increment() { return ++refCount; }
+  };
+
   template <typename T>
   class Sptr {
+    typedef void (Sptr<T>::*unspecified_bool_type)() const;
+    void this_type_does_not_support_comparisons() const {}
+
+    template <typename A> friend class Sptr;
+
     private:
-      class SharedPointer {
-        private:
-          int refCount;
-
-        public:
-          SharedPointer(T& o) : refCount(0), object(o) {}
-          void increment() { ++refCount; }
-          void decrement() { if(--refCount == 0) delete object; }
-
-          T* object;
-      };
+      SharedCounter* counter;
 
     public:
-      SharedPointer* sharedPtr;
+      T* object;
 
-      Sptr();
+      Sptr() : counter(new SharedCounter()) {}
 
       template <typename U>
-      Sptr(U*);
+      Sptr(U* o) : counter(new SharedCounter()), object(static_cast<T*>(o)) {}
 
-      Sptr(const Sptr& s) : sharedPtr(s.sharedPtr) {
-        sharedPtr->increment();
+      Sptr(const Sptr& s) : counter(s.counter), object(s.object) {
+        counter->refCount++;
       };
 
       template <typename U>
-      Sptr(const Sptr<U>& s) : sharedPtr(static_pointer_cast<Sptr<T>>(s).sharedPtr) {
-        sharedPtr->increment();
+      Sptr(const Sptr<U>& s) {
+        object = static_cast<T*>(s.object);
+        counter = static_cast<SharedCounter*>(s.counter);
+        counter->refCount++;
       }
 
       Sptr& operator=(const Sptr& s) {
         if(!(*this == s)) {
-          sharedPtr->decrement();
-          sharedPtr = s.sharedPtr;
+          if(counter->decrement() == 0) delete object;
+          object = s.object;
+          counter = const_cast<SharedCounter*>(s.counter);
         }
-        sharedPtr->increment();
+        counter->refCount++;
         return *this;
       }
 
       template <typename U>
       Sptr<T>& operator=(const Sptr<U>& s) {
-        s = static_pointer_cast<Sptr<T>>(s);
-        sharedPtr->decrement();
-        sharedPtr = s.sharedPtr;
-        sharedPtr->increment();
+        if(counter->decrement() == 0) delete object;
+        object = static_cast<T*>(s.object);
+        counter = const_cast<SharedCounter*>(s.counter);
+        counter->refCount++;
         return *this;
       }
 
-      void reset();
+      void reset() {
+        if(counter->decrement() == 0) delete object;
+        object = 0;
+      }
 
-      T& operator*() const { return *(sharedPtr->object); }
-      T* operator->() const { return sharedPtr->object; }
+      T& operator*() const { return *object; }
+      T* operator->() const { return object; }
 
-      T* get() const { return sharedPtr->object; }
+      T* get() const { return object; }
 
       operator unspecified_bool_type() const {
-        return false; //FIXME
+        return object == 0 ? &Sptr<T>::this_type_does_not_support_comparisons : 0;
       }
   };
 
@@ -66,9 +75,11 @@ namespace cs540 {
 
   template <typename T, typename U>
   Sptr<T> static_pointer_cast(const Sptr<U>& s) {
-    s.sharedPtr->object static_cast<T>(s.)
+    return Sptr<T>(static_cast<T*>(s.object));
   }
 
   template <typename T, typename U>
-  Sptr<T> dynamic_pointer_cast(const Sptr<U>&);
+  Sptr<T> dynamic_pointer_cast(const Sptr<U>& s) {
+    return Sptr<T>(dynamic_cast<T*>(s.object));
+  }
 }
