a6c13d9f5e324073456cb86f3db6213218876679
Andrew Marshall <andrew@johnandrewmarshall.com>
2011-12-16 21:34:27 -0500

    Refactor, store original (derived) object

--- Project3/Part2/Sptr.hpp
+++ Project3/Part2/Sptr.hpp
@@ -1,13 +1,15 @@
 #include <pthread.h>
 
 namespace cs540 {
-  class SharedCounter {
+  class Proxy {
     private:
       int refCount;
       pthread_mutex_t lock;
 
     public:
-      SharedCounter() : refCount(0) { pthread_mutex_init(&lock, 0); }
+      Proxy() : refCount(0) { pthread_mutex_init(&lock, 0); }
+      virtual ~Proxy() {}
+
       int decrement() {
         int r;
         pthread_mutex_lock(&lock);
@@ -19,12 +21,23 @@ namespace cs540 {
         pthread_mutex_unlock(&lock);
         return r;
       }
+
       int increment() {
         pthread_mutex_lock(&lock);
         int r = ++refCount;
         pthread_mutex_unlock(&lock);
         return r;
       }
+  };
+
+  template <typename U>
+  class Proxy_t : public Proxy {
+    private:
+      U* object;
+
+    public:
+      Proxy_t(U* o) : object(o) {}
+      virtual ~Proxy_t() { delete object; }
   };
 
   template <typename T>
@@ -34,50 +47,50 @@ namespace cs540 {
 
     template <typename A> friend class Sptr;
 
-    private:
-      SharedCounter* counter;
-
     public:
       T* object;
 
-      Sptr() : counter(new SharedCounter()) {}
+    private:
+      Proxy* proxy;
+
+    public:
+      Sptr() : object(0), proxy(0) {}
 
       template <typename U>
-      Sptr(U* o) : counter(new SharedCounter()), object(static_cast<T*>(o)) {}
+      Sptr(U* o) : object(o), proxy(new Proxy_t<U>(o)) {}
 
-      Sptr(const Sptr& s) : counter(s.counter), object(s.object) {
-        counter->increment();
+      Sptr(const Sptr& s) : object(s.object), proxy(s.proxy) {
+        proxy->increment();
       };
 
       template <typename U>
-      Sptr(const Sptr<U>& s) {
-        object = static_cast<T*>(s.object);
-        counter = static_cast<SharedCounter*>(s.counter);
-        counter->increment();
+      Sptr(const Sptr<U>& s) : object(static_cast<T*>(s.object)), proxy(s.proxy) {
+        proxy->increment();
       }
 
       Sptr& operator=(const Sptr& s) {
         if(!(*this == s)) {
-          if(object != 0 && counter->decrement() == 0) delete object;
+          if(object != 0 && proxy->decrement() == 0) delete proxy;
           object = s.object;
-          counter = const_cast<SharedCounter*>(s.counter);
+          proxy = s.proxy;
         }
-        counter->increment();
+        proxy->increment();
         return *this;
       }
 
       template <typename U>
       Sptr<T>& operator=(const Sptr<U>& s) {
-        if(object != 0 && counter->decrement() == 0) delete object;
+        if(object != 0 && proxy->decrement() == 0) delete proxy;
         object = static_cast<T*>(s.object);
-        counter = const_cast<SharedCounter*>(s.counter);
-        counter->increment();
+        proxy = s.proxy;
+        proxy->increment();
         return *this;
       }
 
       void reset() {
-        if(object != 0 && counter->decrement() == 0) delete object;
+        if(object != 0 && proxy->decrement() == 0) delete proxy;
         object = 0;
+        proxy = 0;
       }
 
       T& operator*() const { return *object; }
